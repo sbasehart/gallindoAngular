ba83ee65b223c297003513e54f57ae3573798544
Summer Basehart <summer@Judes-Mac-mini.local>
2020-02-19 10:15:47 -0800

    css updates

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -23,7 +23,6 @@ export class AuthGuard implements CanActivate {
     // Store the attempted URL for redirecting
     this.authService.redirectUrl = url;
 
-    // Navigate to the login page with extras
     this.router.navigate(['/login']);
     return false;
   }
