880224df12895fc0914bbb539519ce2d3eed6381
Summer Basehart <summer@Judes-Mac-mini.local>
2020-02-28 11:17:40 -0800

    updated app.js

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname + 'dist')));
+app.use(express.static(path.join(__dirname + 'public')));
 
 app.get('./*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'))
+  res.sendFile(path.join(__dirname + 'public'))
 })
 
 app.use('/api/auth', auth);
